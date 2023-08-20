7a8758471f51ef7de8e94e9c11d34156b17ede1e
Johnny <johnathan.mnn@gmail.com>
2023-08-20 17:07:43 -0500

    On hover 'Johnny' letters change color

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,7 +7,7 @@ export default function Homepage(){
             <header className="container-fluid">
                 <div className="row">
                     <div className="col-12" id="area">
-                        <h1 className="black hello-kooky ms-3">Hi, I'm <span>J</span><span>o</span><span>h</span><span>n</span><span>n</span><span>y</span></h1>
+                        <h1 className="black hello-kooky ms-3">Hi, I'm <span id="j">J</span><span id="o">o</span><span id="h">h</span><span id="n1">n</span><span id="n2">n</span><span id="y">y</span></h1>
                         <h2 className="duper-bold black display-5 ms-3">Creative Developer & Designer</h2>
                             <ul className="circles">
                                 <li></li>
