4cded034165528a27cca95a3c5995be812b8da1a
Johnny <johnathan.mnn@gmail.com>
2023-08-30 12:26:09 -0500

    Overflow-x hidden on safari

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,7 +15,7 @@ export default function About(){
             </header>
             <section>
                 <p className="black duper-regular ">Feel free to shoot me an email or hit me up on LinkedIn. To see more of my works checkout my GitHub!</p>
-             <a href="mailto:contact@johnathanmann.com?subject=Hi Johnny!" data-aos="fade-up" className="duper-bold black display-5">contact@johnathanmann.com</a>
+             <a data-aos="fade-up" href="mailto:contact@johnathanmann.com?subject=Hi Johnny!" className="duper-bold black display-5">contact@johnathanmann.com</a>
                 <div data-aos="fade-up" className="col d-flex my-5 duper-bold">
                     <h2 className="me-3"><FontAwesomeIcon icon={faLinkedin} color="#F9CD54" /><span><a rel="noreferrer" href="https://www.linkedin.com/in/johnathan-mann-03548b240/" target="blank" className="ms-2"> LinkedIn</a></span></h2>
                     <h2><FontAwesomeIcon icon={faGithubSquare} color="#F9CD54" /><span><a rel="noreferrer" href="https://github.com/johnathanmann" target="blank"> GitHub</a></span></h2>
