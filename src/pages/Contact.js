fced274c7d24c732161eaca20ac655a8f136bcab
Johnny <johnathan.mnn@gmail.com>
2023-08-30 11:51:56 -0500

    ADjusted a tag styling and contact page mobile view

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,13 +11,14 @@ export default function About(){
         <main id="contact" className="container-fluid">
             <div id="contact-cover" className="slide-over"></div>
             <header>
-                <h1 data-aos="fade-up" className="yellow hello-kooky ms-5">Lets connect</h1>
+                <h1 data-aos="fade-up" className="yellow hello-kooky">Lets connect</h1>
             </header>
             <section>
-             <a href="mailto:contact@johnathanmann.com?subject=Hi Johnny!"><h2 data-aos="fade-up" className="duper-bold black m-5 display-5">contact@johnathanmann.com</h2></a>
-                <div data-aos="fade-up" className="col d-flex ms-5 my-5 duper-bold">
-                    <h2 className="me-5"><FontAwesomeIcon icon={faLinkedin} color="#F9CD54" /><span><a rel="noreferrer" href="https://www.linkedin.com/in/johnathan-mann-03548b240/" target="blank" className="ms-2">LinkedIn</a></span></h2>
-                    <h2><FontAwesomeIcon icon={faGithubSquare} color="#F9CD54" /><span><a rel="noreferrer" href="https://github.com/johnathanmann" target="blank" className="ms-2">GitHub</a></span></h2>
+                <p className="black duper-regular ">Feel free to shoot me an email or hit me up on LinkedIn. To see more of my works checkout my GitHub!</p>
+             <a href="mailto:contact@johnathanmann.com?subject=Hi Johnny!" data-aos="fade-up" className="duper-bold black display-5">contact@johnathanmann.com</a>
+                <div data-aos="fade-up" className="col d-flex my-5 duper-bold">
+                    <h2 className="me-3"><FontAwesomeIcon icon={faLinkedin} color="#F9CD54" /><span><a rel="noreferrer" href="https://www.linkedin.com/in/johnathan-mann-03548b240/" target="blank" className="ms-2"> LinkedIn</a></span></h2>
+                    <h2><FontAwesomeIcon icon={faGithubSquare} color="#F9CD54" /><span><a rel="noreferrer" href="https://github.com/johnathanmann" target="blank"> GitHub</a></span></h2>
                 </div>
             </section>
         </main>
